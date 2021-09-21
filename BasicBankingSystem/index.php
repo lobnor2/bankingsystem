153503294fdae53ab75587e288124d732904e52f
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2021-09-21 15:02:17 +0530

    Add files via upload

---
+++ BasicBankingSystem/index.php
@@ -0,0 +1,54 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <style>
+      *{
+        font-family: 'Open Sans', sans-serif;     
+       }
+      h1{
+        color:white;
+        font-size:80px;
+        position:absolute;
+        top:50%;
+        left:5%;
+        cursor:pointer;
+        transition:transform .2s ease;
+        /* transition:border 1s linear; */
+      }
+      h1:hover{
+        transform:scale(1.1);
+        /* border:2px solid white; */
+      }
+    </style>
+
+    <title>iBank-Welcome To Best Bank</title>
+  </head>
+
+  <body style="background-color: black;">
+  <header>
+  <?php
+  include 'navbar.php';
+  ?></header>
+<main>
+  <style>
+  body{
+    background-image: url('img/bank.jpeg');
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size:cover;
+    width: 100%;
+    background-position: 400px -50px;
+  }
+</style>
+<h1>Welcome to iBank</h1>
+    </main>
+  </body>
+</html>
---
+++ BasicBankingSystem/navbar.php
@@ -0,0 +1,58 @@
+      <nav class="navbar navbar-expand-md ">
+      <a class="navbar-brand navlogo colorwhite" href="index.php"><b>iBank<svg xmlns="http://www.w3.org/2000/svg" class="svgicon" width="30" height="30" fill="currentColor" class="bi bi-bank2" viewBox="0 0 16 16">
+  <path d="M8.277.084a.5.5 0 0 0-.554 0l-7.5 5A.5.5 0 0 0 .5 6h1.875v7H1.5a.5.5 0 0 0 0 1h13a.5.5 0 1 0 0-1h-.875V6H15.5a.5.5 0 0 0 .277-.916l-7.5-5zM12.375 6v7h-1.25V6h1.25zm-2.5 0v7h-1.25V6h1.25zm-2.5 0v7h-1.25V6h1.25zm-2.5 0v7h-1.25V6h1.25zM8 4a1 1 0 1 1 0-2 1 1 0 0 1 0 2zM.5 15a.5.5 0 0 0 0 1h15a.5.5 0 1 0 0-1H.5z"/>
+</svg>
+</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link colorwhite" href="index.php"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link colorwhite" href="transfermoney.php"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link colorwhite" href="transactionhistory.php"><b>Transaction History</b></a>
+              </li>
+          </div>
+       </nav>
+
+
+       <style>
+         .navbar{
+           background:black;
+           /* opacity: 70%; */
+         }
+         .svgicon{
+           margin-left:15px;
+           font-size:30px;
+           padding-bottom:5px;
+         }
+         .navlogo{
+          font-size:30px;
+          color:white;
+
+
+         }
+         .navlogo:hover{
+           border:none;
+         }
+         .colorwhite{
+           color:white;
+           margin-bottom:0px;
+           transition:border-bottom .5s ease;
+
+           
+         }
+         .colorwhite:hover{
+           border-bottom:1px solid white;
+           color:white !important;
+           /* transform:scale(1.2); */
+
+         }
+         
+
+       </style>
